3c2e78ba8a60f4d77a282ad5e0cc88dfc44d7577
BaluLanka <balasubramanyam.lanka@gmail.com>
2018-05-31 20:00:46 -0400

    Service class

--- src/main/java/com/java/SpringWebApplication/Service/CustomerService.java
+++ src/main/java/com/java/SpringWebApplication/Service/CustomerService.java
@@ -1,5 +1,12 @@
 package com.java.SpringWebApplication.Service;
 
-public interface CustomerService {
+import com.java.SpringWebApplication.DAO.Customers;
 
+public interface CustomerService {
+	Customers createUser(Customers customer);
+	Customers getCustomer(int cust_id);
+	Customers updateCustomer(Customers customer);
+	void deleteCustomer(int cust_id);
+	
+       
 }
---
+++ src/main/java/com/java/SpringWebApplication/Service/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.java.SpringWebApplication.Service;
+
+import com.java.SpringWebApplication.DAO.Customers;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	public Customers createUser(Customers customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Customers getCustomer(int cust_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Customers updateCustomer(Customers customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteCustomer(int cust_id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
