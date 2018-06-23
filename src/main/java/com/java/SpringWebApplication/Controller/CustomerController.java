322b70f13bd671a205fc216c17f3b87e863e4aec
BaluLanka <balasubramanyam.lanka@gmail.com>
2018-06-23 13:31:09 -0400

    modified files

--- src/main/java/com/java/SpringWebApplication/Controller/CustomerController.java
+++ src/main/java/com/java/SpringWebApplication/Controller/CustomerController.java
@@ -2,18 +2,20 @@ package com.java.SpringWebApplication.Controller;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
 import org.hibernate.Session;
 import org.apache.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.java.SpringWebApplication.Service.AddCustomerRequest;
 import com.java.SpringWebApplication.Service.CustomerServiceImpl;
@@ -32,16 +34,25 @@ public class CustomerController {
 	@Autowired
 	AddCustomerRequest cust_req;
 
-	@RequestMapping(value="/create",method = RequestMethod.POST, produces={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
-			consumes={MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	//@RequestMapping(value="/create", method = RequestMethod.POST, produces={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+			//consumes={MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	@PostMapping(path="/create", consumes="application/json")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Response createCustomer(@RequestBody AddCustomerRequest cust_req) {
 		logger.info("controller class");
 		customerServiceImpl.createCustomer(cust_req);
+		
 		logger.info("This is the customer Controller class");
-		response=Response.status(Status.OK).build();
+		//response=Response.status(Status.OK).entity(arg0);
 		return response;
 	}
-
+	@RequestMapping(value="/hello")
+	public String addCustomer()
+      {
+		  logger.info("Hello");
+		return "hello";
+    	  
+      }
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	public String getCustomer(@RequestParam(value = "id") String id) {
 		System.out.println("This is the customer get method");
--- src/main/java/com/java/SpringWebApplication/Service/CustomerServiceImpl.java
+++ src/main/java/com/java/SpringWebApplication/Service/CustomerServiceImpl.java
@@ -5,20 +5,36 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.ws.rs.core.MediaType;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.xml.bind.Marshaller;
+
 
 import com.java.SpringWebApplication.DAO.Customers;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 	Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
+	Marshaller marshal;
 	@Autowired
 	Customers customers;
-
-	public Customers createCustomer(AddCustomerRequest cust_req) {
+		
+	//@RequestMapping(value="/createcustomer", method = RequestMethod.POST, produces={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML},
+			//consumes={MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	@PostMapping(path="/createcustomer", consumes="application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Customers createCustomer(@RequestBody AddCustomerRequest cust_req) {
 		
 			  Set<Customers> customer=new HashSet<Customers>();
 			  customers.setCust_id(cust_req.getCust_id());
@@ -32,8 +48,11 @@ public class CustomerServiceImpl implements CustomerService {
 			  customers.setBilling_State(cust_req.getBilling_State());
 			  customers.setBilling_Country(cust_req.getBilling_Country());
 			  customer.add(customers);
-			  
+			 
               return  customers;
+              
+  			//logger.debug(String.format("Received request: %s", JAXBUtility.marshal(cust_req)));
+
 
 
 		
